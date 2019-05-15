4480cb0be99e3f30585ae8574cfb017ac1d694f0
Hunter <hunter.j.savage@gmail.com>
2019-05-15 15:50:38 -0400

    Add About Us test

---
+++ Features/AboutUs.feature
@@ -0,0 +1,10 @@
+﻿Feature: AboutUs
+
+Scenario: Assert Jax's number matches expected results
+	Given I open Chrome browser yet again
+	Given I navigate to https://www.rothbardchevy.com/ yet again
+	And I go to Meet Our Staff page
+	And I go to Executive Team page
+	When I see Jax Rothbards phone number
+	Then it will match expected
+	Then close browser yet again
---
+++ Steps/AboutUsSteps.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Interactions;
+
+namespace DealerOn_Project.Steps
+{
+    [Binding]
+    public class AboutUsSteps : BaseClass
+    {
+        [Given(@"I open Chrome browser yet again")]
+        public void GivenIOpenChromeBrowserYetAgain()
+        {
+            driver = new ChromeDriver("C:\\Users\\hunte\\source\\repos");
+        }
+
+        [Given(@"I navigate to https://www\.rothbardchevy\.com/ yet again")]
+        public void GivenINavigateToHttpsWww_Rothbardchevy_ComYetAgain()
+        {
+            driver.Navigate().GoToUrl("https://www.rothbardchevy.com/");
+        }
+
+        [Given(@"I go to Meet Our Staff page")]
+        public void GivenIGoToMeetOurStaffPage()
+        {
+            driver.FindElement(By.CssSelector("img[alt='close window']")).Click();
+            driver.SwitchTo().Window(driver.WindowHandles[1]);
+
+            Actions action = new Actions(driver);
+            IWebElement aboutUs = driver.FindElement(By.Id("parent_6"));
+            action.MoveToElement(aboutUs).Perform();
+
+            driver.FindElement(By.Id("7_child_3")).Click();
+        }
+        
+        [Given(@"I go to Executive Team page")]
+        public void GivenIGoToExecutiveTeamPage()
+        {
+            driver.FindElement(By.LinkText("Executive Team")).Click();
+        }
+        
+        [When(@"I see Jax Rothbards phone number")]
+        public void WhenISeeJaxRothbardsPhoneNumber()
+        {
+            IWebElement phoneNumber = driver.FindElement(By.XPath("//*[@id='tab-pane-4417']/div[1]/div[2]/ul/li[3]/ul/li[2]/a"));
+            String phone = phoneNumber.Text;
+            Assert.AreEqual("584-681-5189", phone);
+        }
+
+        [Then(@"it will match expected")]
+        public void ThenItWillMatch()
+        {
+
+        }
+        
+        [Then(@"close browser yet again")]
+        public void ThenCloseBrowserYetAgain()
+        {
+            driver.Quit();
+        }
+    }
+}
